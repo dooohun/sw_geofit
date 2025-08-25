f63c06bbe1778da7c0a2329e274967d02f4d7c30
dooohun <ehgns0305@naver.com>
2025-08-25 14:03:51 +0900

    chore: prettier 설정 추가

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -19,5 +21,11 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...prettierConfig.rules
+    }
   },
 ])
