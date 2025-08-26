3cf9b96d2b1ce1128c09798e1ca1d9d6e75714eb
dooohun <ehgns0305@naver.com>
2025-08-26 12:23:50 +0900

    feat: 챗봇 메인페이지, 상세 분석 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ChatBot from '@/pages/Chatbot';
+import ChatBotHome from '@/pages/Chatbot/Home';
 import Home from '@/pages/Home';
-import './App.css'
+import './App.css';
+import Analysis from '@/pages/Analysis';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/chatbot/:id' element={<ChatBot />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/chatbot" element={<ChatBotHome />} />
+        <Route path="/chatbot/:id" element={<ChatBot />} />
+        <Route path="/analysis" element={<Analysis />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
